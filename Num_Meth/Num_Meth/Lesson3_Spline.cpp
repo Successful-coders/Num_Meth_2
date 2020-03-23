5d255f3c9db9b206939f6f6bc654966ff879b95f
Voronik <voronkina.2018@azure.nstu.ru>
2020-03-23 13:12:39 +0700

    begin

---
+++ Num_Meth/Num_Meth/Lesson3_Spline.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include "Point.h"
+#include "Spline.h"
+#include "Cubic_Interpolation_Spline_1D.h"
+#include "Smoothing_Spline_1D.h"
+
+int main()
+{
+	try
+	{
+		//регулярная равномерная сетка
+		std::vector<Com_Methods::Point> Mesh = { Com_Methods::Point(0.0, 0.0, 0.0),
+												 Com_Methods::Point(1.0, 0.0, 0.0), 
+												 Com_Methods::Point(2.0, 0.0, 0.0) };
+		//значения табличной функции f(x) = x
+		std::vector<double> Func = { 0.0, 1.0, 2.0 };
+
+		//точка для теста
+		Com_Methods::Point Test_Point(1.23, 0.0, 0.0);
+		//для хранения результата
+		std::vector<double> Res(3);
+
+		//кубический интерполяционный сплайн
+		Com_Methods::Cubic_Interpolation_Spline_1D Sp1;
+		//построение сплайна
+		Sp1.Update_Spline(Mesh, Func);
+		//значение сплайна в точке
+		Sp1.Get_Value(Test_Point, &Res[0]);
+		std::cout << "\nCubic Interpolation Spline:\n";
+		std::cout << std::left << std::setw(20) << "x" 
+							   << std::setw(20) << "P(x)" 
+							   << std::setw(20) << "P'(x)"
+							   << std::setw(20) << "P''(x)" << "\n";
+		std::cout << std::left << std::setw(20) << Test_Point.x() 
+							   << std::setw(20) << Res[0] 
+							   << std::setw(20) << Res[1] 
+							   << std::setw(20) << Res[2] << "\n";
+
+		//сглаживающий сплайн
+		Com_Methods::Smoothing_Spline_1D Sp2 (0.0);
+		//построение сплайна
+		Sp2.Update_Spline(Mesh, Func);
+		//значение сплайна в точке
+		Sp2.Get_Value(Test_Point, &Res[0]);
+		std::cout << "\nSmoothing Spline:\n";
+		std::cout << std::left << std::setw(20) << "x" 
+							   << std::setw(20) << "P(x)" 
+							   << std::setw(20) << "P'(x)" << "\n";
+		std::cout << std::left << std::setw(20) << Test_Point.x() 
+							   << std::setw(20) << Res[0] 
+							   << std::setw(20) << Res[1] << "\n";
+
+	}
+	catch (std::exception &Ex)
+	{
+		std::cout << "Error: " << Ex.what() << std::endl;
+	}
+}
---
+++ Num_Meth/Num_Meth/Spline.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include "Point.h"
+namespace Com_Methods
+{
+	//абстрактный класс сплайна
+	class Spline
+	{
+	public:
+		//обновить сплайн
+		virtual void Update_Spline(const std::vector<Point> &Points, const std::vector<double> &F_Value) = 0;
+		//вычислить значение сплайна в точке P
+		virtual void Get_Value(const Point &P, double * Res)const = 0;
+	};
+}
